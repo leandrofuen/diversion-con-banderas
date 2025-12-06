c3c11104dcafa189d65def7f89d7864daddb3662
leandrofuen <leandro_fuba@hotmail.es>
2025-12-06 14:06:08 +0000

    ya tenemos banderas

--- js/script.js
+++ js/script.js
@@ -10,35 +10,37 @@ console.log("funciona")
 // - capital
 
 const countrieslist= document.getElementById("countries-list")
-
+ container= document.getElementById("listapaises")
 
 const conseguirbandera = async ()=>{
 try {
   const response = await fetch("https://restcountries.com/v3.1/all?fields=name,flags,car,population,capital")
-  if (!response.ok){
-    throw new error ("ha habido un error",response.status)
+      if (!response.ok){
+        throw new Error ("ha habido un error",response.status)
   }
   const data = await response.json()
-  return {
-  name: data.name,
-  flags: data.flags,
-  car: data.car,
-  population : data.population,
-  };
+  console.log("este es el data con todo para mirarlo",data)
+  const PAISESORDENADOS= data.map (pais=>pais.name.common).sort((a,b)=>a.localeCompare(b));
+
+  console.log(PAISESORDENADOS)
+
+  PAISESORDENADOS.forEach(paisnombre => {
+    const pais = data.find(p=>p.name.common===paisnombre)
+   
+
+let templatebandera=`<li><img src="${pais.flags.png}"><br>PAIS:${paisnombre} </li>`
+container.innerHTML+= templatebandera  ;
+
+  });
+
+
+  
 
 }catch (error){
   console.log("error al obtener datos", error)
 }
 };
-(async () => {
-        try {
-
-const template =(id)=>{
-    let templatebande =`<li>${id.name}</li>`
-}
-        
-}
-    }
 
 
-    conseguirbandera().then((data)=>template(data))
+
+   conseguirbandera()
